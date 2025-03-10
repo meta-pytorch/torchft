f7794a2749a5f0882310c079a66289986ad1321d
Dave Lei <davelei@meta.com>
2025-03-10 07:42:45 -0700

    Fix init_sync logic
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchft/_torchft.pyi
+++ torchft/_torchft.pyi
@@ -11,7 +11,7 @@ class ManagerClient:
         checkpoint_metadata: str,
         shrink_only: bool,
         timeout: timedelta,
-        init_sync: Optional[bool] = False,
+        init_sync: bool = True,
     ) -> QuorumResult: ...
     def _checkpoint_metadata(self, rank: int, timeout: timedelta) -> str: ...
     def should_commit(
