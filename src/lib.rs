73e66939cdac3295260a03b0db3ccb1b0d1f44d5
fduwjj <fduwjj@gmail.com>
2025-04-02 13:48:00 -0700

    Remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,6 @@ use crate::torchftpb::{
 };
 use pyo3::prelude::*;
 use pyo3::types::{PyDict, PyString};
-use pyo3::wrap_pyfunction;
 
 // Get the number of threads to use for the tokio runtime
 fn num_threads() -> usize {
