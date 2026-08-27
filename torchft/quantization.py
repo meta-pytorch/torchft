667e91da85aaf5df7d0932edb1a065162ba7f2c7
Maggie Moss <maggiemoss@meta.com>
2026-08-27 16:51:56 -0700

    Restore Pyre mode header (#351)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchft/pull/351
    
    Revert the TorchFT portion of D117751289 by restoring the `# pyre-unsafe` header in `torchft/quantization.py`.
    
    ___
    
    Differential Revision: D117785789
    
    fbshipit-source-id: 4bd09470a630220054141a874ab3e2fa4cedd51a

--- torchft/quantization.py
+++ torchft/quantization.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
 import torch
 
 # pyre-ignore[21]: Could not find a module corresponding to import `triton`
